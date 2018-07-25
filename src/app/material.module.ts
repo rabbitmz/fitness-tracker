22b7db7b1dfaadab1626296af4d178c8da3146e4
rabbitmz <yuri2jwingeter@gmail.com>
2018-07-25 23:32:05 +0200

    first button

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,9 @@
 // a class to help us manager the imports in needed for angular material
 import { NgModule } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatButtonModule, MatIconModule],
+  exports: [MatButtonModule, MatIconModule]
   })
 export class MaterialModule {}

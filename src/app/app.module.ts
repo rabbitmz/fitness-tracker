6b89982aca28d9277182f954ec678571eff1b97f
rabbitmz <yuri2jwingeter@gmail.com>
2018-07-25 21:30:40 +0200

    initial Project Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,8 @@
+// a class to help us manager the imports in needed for angular material
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [],
+  exports: []
+  })
+export class MaterialModule {}

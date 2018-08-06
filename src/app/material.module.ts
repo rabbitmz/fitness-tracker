81d2f3eba5cb72dfc6f5312242bf3a8308592e9c
rabbitmz <yuri2jwingeter@gmail.com>
2018-08-06 23:16:48 +0200

    navbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,33 @@
 // a class to help us manager the imports in needed for angular material
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule
+} from '@angular/material';
+
+
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [
+            MatButtonModule, MatIconModule,
+            MatFormFieldModule, MatCheckboxModule,
+            MatInputModule, MatDatepickerModule,
+            MatNativeDateModule, MatSidenavModule,
+            MatToolbarModule, MatListModule
+          ],
+  exports: [
+            MatButtonModule, MatIconModule, MatFormFieldModule,
+            MatInputModule, MatDatepickerModule,
+            MatNativeDateModule, MatCheckboxModule,
+            MatSidenavModule, MatToolbarModule, MatListModule
+          ]
   })
 export class MaterialModule {}
